10d2ed982e247e15d77b5d901fd35ebe2efb863a
thekranker <karannkathuria@gmail.com>
2024-06-24 21:28:55 -0700

    Initial commit

---
+++ src/main/java/net/bananaman532/extrahealthmod/ExtraHealthModClient.java
@@ -0,0 +1,10 @@
+package net.bananaman532.extrahealthmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtraHealthModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
